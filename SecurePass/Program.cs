e7dd25ef1f29b64558360a528093be7c94f453a8
Javier Guzman Cruz <skullowner83@gmail.com>
2025-11-05 18:35:14 -0600

    feat: Create an endpoint to validate the password strength.

--- SecurePass/Program.cs
+++ SecurePass/Program.cs
@@ -1,6 +1,5 @@
 using SecurePass.Models;
-using System.Diagnostics;
-using System.Text;
+using System.Text.RegularExpressions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -31,18 +30,16 @@ app.MapPost("/generate", (Generator generator) => {
 	char[] result = new char[generator.Length];
 	string chars = string.Empty;
 
-	if (generator.Length < 0 || generator.Length > 100) {
+	if (generator.Length < 0 || generator.Length > 100)
 		return Results.BadRequest("Specified length out of range.");
-	}
 
 	if (generator.Upper) chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	if (generator.Lower) chars += "abcdefghijklmnopqrstuvwxyz";
 	if (generator.Numbers) chars += "0123456789";
 	if (generator.Symbols) chars += "!@#$%^&*()-_=+[]{};:,.<>/?|\\`~";
 
-	if (string.IsNullOrEmpty(chars)) {
+	if (string.IsNullOrEmpty(chars))
 		return Results.BadRequest("No character groups gave been provided to generate the password.");
-	}
 
 	for(int i = 0; i < result.Length; i++)
 	{
@@ -52,3 +49,30 @@ app.MapPost("/generate", (Generator generator) => {
 	string generatedPassword = new string(result);
 	return Results.Ok(generatedPassword);
 });
+
+app.MapPost("/validate", (string password) =>
+{
+	if (string.IsNullOrEmpty(password))
+		return Results.BadRequest("Password cannot be empty.");
+
+	int score = 0;
+
+	if (password.Any(char.IsUpper)) score++;
+	if (password.Any(char.IsNumber)) score++;
+	if (password.Any(c => char.IsSymbol(c) || char.IsPunctuation(c))) score++;
+	if (password.Length >= 12) score++;
+	if (Regex.IsMatch(password, @"(abc|123|qwe|password|admin)", RegexOptions.IgnoreCase)) score -= 2;
+
+	string strength = score switch
+	{
+		1 => "Weak",
+		2 => "Medium",
+		3 => "Strong",
+		4 => "Very strong",
+		_ => "Unknown"
+	};
+
+	return Results.Ok(strength);
+});
+
+app.Run();
